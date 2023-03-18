4fa8cdd04143b0a49bd265c331cdbac5d859cfbb
Rolarg29 <rolurmzguerra@gmail.com>
2023-03-17 19:06:47 -0600

    implemented static methods

--- src/test/java/a1qa/patterns/BaseUtil.java
+++ src/test/java/a1qa/patterns/BaseUtil.java
@@ -12,50 +12,50 @@ import java.time.Duration;
 import java.util.List;
 
 public class BaseUtil {
-    private WebDriver driver;
+    private static WebDriver driver;
     public BaseUtil(WebDriver driver) {
-        this.driver = driver;
+        BaseUtil.driver = driver;
     }
 
     //webdriver setup
-    public WebDriver chromeDriverConnection() {
+    public static WebDriver chromeDriverConnection() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         return driver;
     }
 
     //finds an element by any locator given
-    public WebElement findElement(By locator) {
+    public static WebElement findElement(By locator) {
         return driver.findElement(locator);
     }
 
     //returns a list of all elements with the given locator
-    public List<WebElement> findElements(By locator) {
+    public static List<WebElement> findElements(By locator) {
         return driver.findElements(locator);
     }
 
     //Get the text of the element
-    public String getText(WebElement element) {
+    public static String getText(WebElement element) {
         return element.getText();
     }
 
     //Get the text of the element using a locator
-    public String getText(By locator) {
+    public static String getText(By locator) {
         return driver.findElement(locator).getText();
     }
 
     //type text in element or input
-    public void type(String inputText, By locator) {
+    public static void type(String inputText, By locator) {
         driver.findElement(locator).sendKeys(inputText);
     }
 
     //clicks on element
-    public void click(By locator) {
+    public static void click(By locator) {
         driver.findElement(locator).click();
     }
 
     //checks if element is displayed
-    public boolean isDisplayed(By locator) {
+    public static boolean isDisplayed(By locator) {
         try{
             return driver.findElement(locator).isDisplayed();
         } catch (org.openqa.selenium.NoSuchElementException e) {
@@ -64,18 +64,18 @@ public class BaseUtil {
     }
 
     //initializes the browser into a website
-    public void visit(String url) {
+    public static void visit(String url) {
         driver.get(url);
     }
 
     //wait for an element to be displayed
-    public void waitForElement(By locator) {//creado por mi
+    public static void waitForElement(By locator) {//creado por mi
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     //Scrolls down to find element
-    public void scrollTo(By locator) {//creado por mi
+    public static void scrollTo(By locator) {//creado por mi
         WebElement element = driver.findElement(locator);
         new Actions(driver)
                 .scrollToElement(element)
